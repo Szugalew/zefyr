b82350ed777f66453f66e2931ca2f696ee28e342
Mikhail <44127527+Szugalew@users.noreply.github.com>
2020-07-13 22:03:39 -0400

    Update image.dart

--- packages/zefyr/lib/src/widgets/image.dart
+++ packages/zefyr/lib/src/widgets/image.dart
@@ -2,42 +2,41 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 import 'dart:async';
+import 'dart:io';
+import 'dart:math' as math;
 import 'dart:ui' as ui;
 
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:meta/meta.dart';
 import 'package:notus/notus.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'editable_box.dart';
-import 'selection_utils.dart';
-import 'theme.dart';
 
-/// Provides interface for embedding images into Zefyr editor.
-// TODO: allow configuring image sources and related toolbar buttons.
-@experimental
 abstract class ZefyrImageDelegate<S> {
-  /// Unique key to identify camera source.
-  S get cameraSource;
-
-  /// Unique key to identify gallery source.
-  S get gallerySource;
-
-  /// Builds image widget for specified image [key].
-  ///
-  /// The [key] argument contains value which was previously returned from
-  /// [pickImage] method.
-  Widget buildImage(BuildContext context, String key);
+  /// Creates [ImageProvider] for specified [imageSource].
+  ImageProvider createImageProvider(String imageSource);
 
   /// Picks an image from specified [source].
   ///
   /// Returns unique string key for the selected image. Returned key is stored
   /// in the document.
-  ///
-  /// Depending on your application returned key may represent a path to
-  /// an image file on user's device, an HTTP link, or an identifier generated
-  /// by a file hosting service like AWS S3 or Google Drive.
   Future<String> pickImage(S source);
+}
+
+class ZefyrDefaultImageDelegate implements ZefyrImageDelegate<ImageSource> {
+  @override
+  ImageProvider createImageProvider(String imageSource) {
+    final file = new File.fromUri(Uri.parse(imageSource));
+    return new FileImage(file);
+  }
+
+  @override
+  Future<String> pickImage(ImageSource source) async {
+    final file = await ImagePicker.pickImage(source: source);
+    if (file == null) return null;
+    return file.uri.toString();
+  }
 }
 
 class ZefyrImage extends StatefulWidget {
@@ -52,22 +51,64 @@ class ZefyrImage extends StatefulWidget {
 }
 
 class _ZefyrImageState extends State<ZefyrImage> {
-  String get imageSource {
+  ImageProvider _provider;
+  ImageStream _imageStream;
+  ImageInfo _imageInfo;
+
+  @override
+  void initState() {
+    super.initState();
     EmbedAttribute attribute = widget.node.style.get(NotusAttribute.embed);
-    return attribute.value['source'] as String;
+    final source = attribute.value['source'];
+    _provider = widget.delegate.createImageProvider(source);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _getImage();
+  }
+
+  @override
+  void didUpdateWidget(ZefyrImage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    EmbedAttribute oldStyle = oldWidget.node.style.get(NotusAttribute.embed);
+    final oldSource = oldStyle.value['source'];
+    EmbedAttribute style = widget.node.style.get(NotusAttribute.embed);
+    final source = style.value['source'];
+    if (oldSource != source || oldWidget.delegate != widget.delegate) {
+      _provider = widget.delegate.createImageProvider(source);
+      _getImage();
+    }
+  }
+
+  @override
+  void dispose() {
+    _imageStream.removeListener(_updateImage);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = ZefyrTheme.of(context);
-    final image = widget.delegate.buildImage(context, imageSource);
     return _EditableImage(
-      child: Padding(
-        padding: theme.defaultLineTheme.padding,
-        child: image,
-      ),
+      child: new RawImage(image: _imageInfo?.image),
       node: widget.node,
     );
+  }
+
+  void _getImage() {
+    final oldImageStream = _imageStream;
+    _imageStream = _provider.resolve(createLocalImageConfiguration(context));
+    if (_imageStream.key != oldImageStream?.key) {
+      oldImageStream?.removeListener(_updateImage);
+      _imageStream.addListener(_updateImage);
+    }
+  }
+
+  void _updateImage(ImageInfo imageInfo, bool synchronousCall) {
+    setState(() {
+      _imageInfo = imageInfo;
+    });
   }
 }
 
@@ -80,7 +121,7 @@ class _EditableImage extends SingleChildRenderObjectWidget {
 
   @override
   RenderEditableImage createRenderObject(BuildContext context) {
-    return RenderEditableImage(node: node);
+    return new RenderEditableImage(node: node);
   }
 
   @override
@@ -91,121 +132,104 @@ class _EditableImage extends SingleChildRenderObjectWidget {
 }
 
 class RenderEditableImage extends RenderBox
-    with RenderObjectWithChildMixin<RenderBox>, RenderProxyBoxMixin<RenderBox>
+    with
+        RenderObjectWithChildMixin<RenderImage>,
+        RenderProxyBoxMixin<RenderImage>
     implements RenderEditableBox {
+  static const kPaddingBottom = 24.0;
+
   RenderEditableImage({
     RenderImage child,
     @required EmbedNode node,
-  }) : node = node {
+  }) : _node = node {
     this.child = child;
   }
 
   @override
-  EmbedNode node;
+  EmbedNode get node => _node;
+  EmbedNode _node;
+  void set node(EmbedNode value) {
+    _node = value;
+  }
 
-  // TODO: Customize caret height offset instead of adjusting here by 2px.
   @override
-  double get preferredLineHeight => size.height + 2.0;
+  double get preferredLineHeight => size.height;
 
   @override
-  SelectionOrder get selectionOrder => SelectionOrder.foreground;
+  SelectionOrder get selectionOrder => SelectionOrder.background;
 
   @override
   TextSelection getLocalSelection(TextSelection documentSelection) {
     if (!intersectsWithSelection(documentSelection)) return null;
 
-    final nodeBase = node.documentOffset;
-    final nodeExtent = nodeBase + node.length;
-    return selectionRestrict(nodeBase, nodeExtent, documentSelection);
+    int nodeBase = node.documentOffset;
+    int nodeExtent = nodeBase + node.length;
+    int base = math.max(0, documentSelection.baseOffset - nodeBase);
+    int extent =
+        math.min(documentSelection.extentOffset, nodeExtent) - nodeBase;
+    return documentSelection.copyWith(baseOffset: base, extentOffset: extent);
   }
 
   @override
   List<ui.TextBox> getEndpointsForSelection(TextSelection selection) {
-    final local = getLocalSelection(selection);
+    TextSelection local = getLocalSelection(selection);
     if (local.isCollapsed) {
-      final dx = local.extentOffset == 0 ? _childOffset.dx : size.width;
+      final dx = local.extentOffset == 0 ? 0.0 : size.width;
       return [
-        ui.TextBox.fromLTRBD(dx, 0.0, dx, size.height, TextDirection.ltr),
+        new ui.TextBox.fromLTRBD(dx, 0.0, dx, size.height, TextDirection.ltr),
       ];
     }
 
-    final rect = _childRect;
     return [
-      ui.TextBox.fromLTRBD(
-          rect.left, rect.top, rect.left, rect.bottom, TextDirection.ltr),
-      ui.TextBox.fromLTRBD(
-          rect.right, rect.top, rect.right, rect.bottom, TextDirection.ltr),
+      new ui.TextBox.fromLTRBD(0.0, 0.0, 0.0, size.height, TextDirection.ltr),
+      new ui.TextBox.fromLTRBD(
+          size.width, 0.0, size.width, size.height, TextDirection.ltr),
     ];
   }
 
   @override
   TextPosition getPositionForOffset(Offset offset) {
-    var position = node.documentOffset;
+    int position = _node.documentOffset;
 
     if (offset.dx > size.width / 2) {
       position++;
     }
-    return TextPosition(offset: position);
+    return new TextPosition(offset: position);
   }
 
   @override
   TextRange getWordBoundary(TextPosition position) {
-    final start = node.documentOffset;
-    return TextRange(start: start, end: start + 1);
+    final start = _node.documentOffset;
+    return new TextRange(start: start, end: start + 1);
   }
 
   @override
   bool intersectsWithSelection(TextSelection selection) {
-    final base = node.documentOffset;
-    final extent = base + node.length;
-    return selectionIntersectsWith(base, extent, selection);
+    final int base = node.documentOffset;
+    final int extent = base + node.length;
+    return base <= selection.extentOffset && selection.baseOffset <= extent;
   }
 
   @override
   Offset getOffsetForCaret(TextPosition position, Rect caretPrototype) {
     final pos = position.offset - node.documentOffset;
-    var caretOffset = _childOffset - Offset(kHorizontalPadding, 0.0);
+    Offset caretOffset = Offset.zero;
     if (pos == 1) {
-      caretOffset =
-          caretOffset + Offset(_lastChildSize.width + kHorizontalPadding, 0.0);
+      caretOffset = caretOffset + new Offset(size.width - 1.0, 0.0);
     }
     return caretOffset;
   }
 
   @override
-  void paintSelection(PaintingContext context, Offset offset,
-      TextSelection selection, Color selectionColor) {
+  void paintSelection(PaintingContext context, ui.Offset offset,
+      TextSelection selection, ui.Color selectionColor) {
     final localSelection = getLocalSelection(selection);
     assert(localSelection != null);
     if (!localSelection.isCollapsed) {
-      final paint = Paint()
-        ..color = selectionColor
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 3.0;
-      final rect =
-          Rect.fromLTWH(0.0, 0.0, _lastChildSize.width, _lastChildSize.height);
-      context.canvas.drawRect(rect.shift(offset + _childOffset), paint);
+      final Paint paint = new Paint()..color = selectionColor;
+      final rect = new Rect.fromLTWH(0.0, 0.0, size.width, size.height);
+      context.canvas.drawRect(rect.shift(offset), paint);
     }
-  }
-
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    super.paint(context, offset + _childOffset);
-  }
-
-  static const double kHorizontalPadding = 1.0;
-
-  Size _lastChildSize;
-
-  Offset get _childOffset {
-    final dx = (size.width - _lastChildSize.width) / 2 + kHorizontalPadding;
-    final dy = (size.height - _lastChildSize.height) / 2;
-    return Offset(dx, dy);
-  }
-
-  Rect get _childRect {
-    return Rect.fromLTWH(_childOffset.dx, _childOffset.dy, _lastChildSize.width,
-        _lastChildSize.height);
   }
 
   @override
@@ -213,16 +237,11 @@ class RenderEditableImage extends RenderBox
     assert(constraints.hasBoundedWidth);
     if (child != null) {
       // Make constraints use 16:9 aspect ratio.
-      final width = constraints.maxWidth - kHorizontalPadding * 2;
       final childConstraints = constraints.copyWith(
-        minWidth: 0.0,
-        maxWidth: width,
-        minHeight: 0.0,
-        maxHeight: (width * 9 / 16).floorToDouble(),
+        maxHeight: (constraints.maxWidth * 9 / 16).floorToDouble(),
       );
       child.layout(childConstraints, parentUsesSize: true);
-      _lastChildSize = child.size;
-      size = Size(constraints.maxWidth, _lastChildSize.height);
+      size = new Size(child.size.width, child.size.height + kPaddingBottom);
     } else {
       performResize();
     }
